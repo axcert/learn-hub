5b31f1741ec9953a94d2ee54ae1024464932a70c
tharinduaxcertro <tharindu.herath@axcertro.com>
2024-05-14 13:36:33 +0530

    feat: Added link for student dashboard.
    Resolves #20

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -26,6 +26,11 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink href={route('students.index')} active={route().current('students.index')}>
+                                    Student Dashboard
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
