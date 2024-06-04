4cf5399b4e3d7265217ebb182a0486e57e96de33
TharinduDananjaya <herathhmtd.20@uom.lk>
2024-06-04 15:28:06 +0530

    bug: fixed bug with build.

--- app/Http/Controllers/TeachersArea/Service/TeacherServiceController.php
+++ app/Http/Controllers/TeachersArea/Service/TeacherServiceController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Service;
 use App\Http\Requests\StoreServiceRequest;
 use App\Http\Requests\UpdateServiceRequest;
