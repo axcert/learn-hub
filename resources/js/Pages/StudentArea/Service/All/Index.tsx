dcc007f7370e695d165b7a4807775941cd5045c4
TharinduDananjaya <herathhmtd.20@uom.lk>
2024-06-11 17:32:22 +0530

    feat: Implemented some changes with merge.

--- resources/js/Pages/StudentArea/Service/All/Index.tsx
+++ resources/js/Pages/StudentArea/Service/All/Index.tsx
@@ -6,12 +6,15 @@ import MyDialog from '@/Components/MyDialog/MyDialog';
 import { Service, User, Filters } from '@/types';
 import Card from '@/Components/Card/Card';
 import SearchBar from '@/Components/SearchBar/SearchBar';
+import ReactPaginate from 'react-paginate';
 
 interface Props {
     services: Service[];
     teachersCount: number;
     filters: Filters;
 }
+
+const ITEMS_PER_PAGE = 20;
 
 export default function ServiceIndex({
     services = [],
@@ -20,6 +23,9 @@ export default function ServiceIndex({
 }: Props & { search?: string }) {
     const { auth } = usePage().props as unknown as { auth: { user: User } };
     const [searchTerm, setSearchTerm] = useState<string>(search || "");
+    const [selectedService, setSelectedService] = useState<Service | null>(null);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
 
     const handleDelete = (id: number) => {
         if (confirm("Are you sure you want to delete this service?")) {
@@ -36,12 +42,31 @@ export default function ServiceIndex({
                     .includes(searchTerm.toLowerCase()))
     );
 
+    const pageCount = Math.ceil(filteredServices.length / ITEMS_PER_PAGE);
+    const offset = currentPage * ITEMS_PER_PAGE;
+    const currentItems = filteredServices.slice(offset, offset + ITEMS_PER_PAGE);
+
+    const handlePageClick = ({ selected }: { selected: number }) => {
+        setCurrentPage(selected);
+    };
+
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value);
+        setCurrentPage(0);
     };
 
     const handleSearchClick = () => {
-        // setCurrentPage(0);
+        setCurrentPage(0);
+    };
+
+    const openDialog = (service: Service) => {
+        setSelectedService(service);
+        setIsDialogOpen(true);
+    };
+
+    const closeDialog = () => {
+        setSelectedService(null);
+        setIsDialogOpen(false);
     };
 
     return (
@@ -89,22 +114,19 @@ export default function ServiceIndex({
                         </div>
                     </div>
                     <div className="mt-10 grid gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-4">
-                        {Array.isArray(filteredServices) &&
-                        filteredServices.length > 0 ? (
-                            filteredServices.map((service) => (
-                                <Link
-                                    href={route(
-                                        "student.services.show",
-                                        service.id
-                                    )}
+                        {Array.isArray(currentItems) &&
+                        currentItems.length > 0 ? (
+                            currentItems.map((service) => (
+                                <div
+                                    onClick={() => openDialog(service)}
                                     key={service.id}
-                                    className="flex flex-col items-center bg-white border border-gray-200 rounded-lg p-6 hover:bg-gray-100 transition"
+                                    className="cursor-pointer flex flex-col items-center bg-white border border-gray-200 rounded-lg p-6 hover:bg-gray-100 transition"
                                 >
                                     <div className="flex-shrink-0">
                                         <img
-                                            className="h-16 w-16 rounded-full"
-                                            src="https://cdn-icons-png.flaticon.com/512/4762/4762311.png"
-                                            alt={service.name}
+                                          src={service.image ? `/storage/${service.image}` : "https://cdn-icons-png.flaticon.com/512/4762/4762311.png"}
+                                          alt={service.name}
+                                          className="h-16 w-16 rounded-full"
                                         />
                                     </div>
                                     <div className="mt-4 text-center">
@@ -124,7 +146,7 @@ export default function ServiceIndex({
                                             Rs: {service.hourly_rate}/hr
                                         </p>
                                     </div>
-                                </Link>
+                                </div>
                             ))
                         ) : (
                             <p className="text-center col-span-full text-gray-500">
@@ -132,19 +154,79 @@ export default function ServiceIndex({
                             </p>
                         )}
                     </div>
-                    {Array.isArray(filteredServices) &&
-                        filteredServices.length > 3 && (
-                            <div className="flex justify-center mt-4">
-                                <Link
-                                    href={route("student.services.index")}
-                                    className="text-blue-500 hover:text-blue-700"
-                                >
-                                    View More
-                                </Link>
-                            </div>
-                        )}
+                    <div className="mt-4 flex justify-center">
+                        <ReactPaginate
+                            previousLabel={"← Previous"}
+                            nextLabel={"Next →"}
+                            breakLabel={"..."}
+                            pageCount={pageCount}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={5}
+                            onPageChange={handlePageClick}
+                            containerClassName={
+                                "flex justify-center items-center space-x-2 mt-4"
+                            }
+                            pageClassName={"mx-1"}
+                            pageLinkClassName={
+                                "px-3 py-1 border border-gray-300 rounded-md text-gray-700 hover:bg-blue-500 hover:text-white"
+                            }
+                            previousLinkClassName={
+                                "px-3 py-1 border border-gray-300 rounded-md text-gray-700 hover:bg-blue-500 hover:text-white"
+                            }
+                            nextLinkClassName={
+                                "px-3 py-1 border border-gray-300 rounded-md text-gray-700 hover:bg-blue-500 hover:text-white"
+                            }
+                            breakLinkClassName={
+                                "px-3 py-1 border border-gray-300 rounded-md text-gray-700"
+                            }
+                            activeClassName={"bg-blue-500 rounded-md text-white"}
+                        />
+                    </div>
                 </div>
             </div>
+
+            <MyDialog
+                isOpen={isDialogOpen}
+                setIsOpen={setIsDialogOpen}
+                className={
+                    "inline-block w-full max-w-lg p-2 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-lg"
+                }
+            >
+                {selectedService && (
+                    <div>
+                        {selectedService.image && (
+                            <div className="flex justify-center mt-4">
+                                <img
+                                    src={
+                                        selectedService.image
+                                            ? `/storage/${selectedService.image}`
+                                            : "https://cdn-icons-png.flaticon.com/512/4762/4762311.png"
+                                    }
+                                    alt={selectedService.name}
+                                    className="h-28 w-28 rounded-full"
+                                />
+                            </div>
+                        )}
+                        <h2 className="text-xl font-bold">{selectedService.name}</h2>
+                        {selectedService.teacher && (
+                            <p className="mt-2">Teacher: {selectedService.teacher.user.name}</p>
+                        )}
+                        <p className="mt-2">{selectedService.description}</p>
+                        <p className="text-gray-700 mb-2">Experience: {selectedService.experience}</p>
+                        <p className="mt-2">Hourly Rate: Rs {selectedService.hourly_rate}</p>
+                        <div className="flex justify-center items-center mt-6">
+                            <Link
+                                href={route("student.bookings.create", {
+                                    service_id: selectedService.id,
+                                })}
+                                className="text-blue-600 hover:text-blue-900"
+                            >
+                                Book Service
+                            </Link>
+                        </div>
+                    </div>
+                )}
+            </MyDialog>
         </StudentLayout>
     );
 }
