79b55e46f7d77dfafa36d46869b360e3f34d4f8b
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-06-27 17:52:30 +0530

    feat: create new message function student

--- resources/js/Pages/StudentArea/Chat/All/Index.tsx
+++ resources/js/Pages/StudentArea/Chat/All/Index.tsx
@@ -8,7 +8,6 @@ import { BsFillSendFill } from "react-icons/bs";
 interface Chat {
     teacher: {
         user: {
-            image: string | undefined;
             name: string;
             email: string;
         };
@@ -23,7 +22,6 @@ interface Props {
     onSelectChat: (chatId: number) => void;
 }
 
-// chat sidebar
 const ChatSidebar: React.FC<Props> = ({ chats, onSelectChat }) => {
     return (
         <div className="w-full lg:w-1/4 border-r">
@@ -83,16 +81,127 @@ const ChatSidebar: React.FC<Props> = ({ chats, onSelectChat }) => {
     );
 };
 
-// main
-export default function Index({ auth, chats, message }: PageProps) {
-    console.log("message : ", message);
-    console.log("chats : ", chats);
+interface Message {
+    id: number;
+    chat_id: number;
+    message: string;
+    sender: "teacher" | "student";
+    timestamp: string;
+}
 
+interface ChatMessagesProps {
+    chatId: number;
+    chats: Chat[];
+    initialMessages: Message[];
+}
+
+const ChatMessages: React.FC<ChatMessagesProps> = ({
+    chatId,
+    chats,
+    initialMessages,
+}) => {
+    const [messages, setMessages] = useState<Message[]>(initialMessages);
+    const [newMessage, setNewMessage] = useState("");
+
+    const handleSendMessage = (event: React.FormEvent) => {
+        event.preventDefault();
+        const timestamp = new Date().toISOString();
+        const newMsg: Message = {
+            id: messages.length + 1,
+            chat_id: chatId,
+            message: newMessage,
+            sender: "student",
+            timestamp: timestamp,
+        };
+
+        setMessages([...messages, newMsg]);
+        setNewMessage("");
+    };
+
+    const chat = chats.find((chat) => chat.id === chatId);
+
+    return (
+        <div className="flex flex-col h-full">
+            <div className="p-2 font-bold">
+                <h2>Messages</h2>
+            </div>
+            <div className="flex-grow overflow-y-auto">
+                <ul className="p-5 overflow-y-scroll max-h-80">
+                    {messages.map((message) => (
+                        <li key={message.id} className="py-2 px-4">
+                            {message.sender === "student" ? (
+                                <div className="flex justify-end">
+                                    <div className="max-w-60">
+                                        <p className="font-bold text-sm text-left">
+                                            Sender: Student
+                                        </p>
+                                        <div className="p-4 bg-gray-200 max-w-64 rounded-xl">
+                                            <p>{message.message}</p>
+                                        </div>
+                                        <p className="text-xs text-gray-400 mt-1">
+                                            {new Date(
+                                                message.timestamp
+                                            ).toLocaleString()}
+                                        </p>
+                                    </div>
+                                </div>
+                            ) : message.sender === "teacher" ? (
+                                <div className="flex items-start">
+                                    <div className="max-w-60">
+                                        <p className="font-bold text-sm">
+                                            Receiver: {chat?.teacher.user.name}
+                                        </p>
+                                        <div className="p-4 bg-blue-200 max-w-64 rounded-xl">
+                                            <p>{message.message}</p>
+                                        </div>
+                                        <p className="text-xs text-gray-400 mt-1">
+                                            {new Date(
+                                                message.timestamp
+                                            ).toLocaleString()}
+                                        </p>
+                                    </div>
+                                </div>
+                            ) : null}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="mt-5">
+                <form onSubmit={handleSendMessage}>
+                    <label htmlFor="chat" className="sr-only">
+                        Your message
+                    </label>
+                    <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+                        <textarea
+                            id="chat"
+                            role="1"
+                            className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Your message..."
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                        ></textarea>
+                        <button
+                            type="submit"
+                            className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+                        >
+                            <div>
+                                <BsFillSendFill className="size-5" />
+                            </div>
+                            <span className="sr-only">Send message</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+const Index: React.FC<PageProps> = ({ auth, chats, message }) => {
     const [selectedChatId, setSelectedChatId] = useState<number | null>(null);
 
     const handleSelectChat = (chatId: number) => {
         setSelectedChatId(chatId);
-        console.log(selectedChatId);
     };
 
     return (
@@ -125,113 +234,6 @@ export default function Index({ auth, chats, message }: PageProps) {
             </div>
         </StudentLayout>
     );
-}
-
-interface Message {
-    id: number;
-    chat_id: number;
-    message: string;
-    sender: "teacher" | "student";
-    timestamp: string;
-}
-
-interface ChatMessagesProps {
-    chatId: number;
-    chats: Chat[];
-    initialMessages: Message[];
-}
-
-// chat message
-const ChatMessages: React.FC<ChatMessagesProps> = ({
-    chatId,
-    chats,
-    initialMessages,
-}) => {
-    const [messages, setMessages] = useState<Message[]>(initialMessages);
-    const [newMessage, setNewMessage] = useState("");
-
-    const handleSendMessage = (event: React.FormEvent) => {
-        event.preventDefault();
-        const timestamp = new Date().toISOString();
-        const newMsg: Message = {
-            id: messages.length + 1,
-            chat_id: chatId,
-            message: newMessage,
-            sender: "student",
-            timestamp: timestamp,
-        };
-
-        setMessages([...messages, newMsg]);
-        setNewMessage("");
-    };
-
-    const chat = chats.find((chat) => chat.id === chatId);
-
-    return (
-        <div>
-            <div className="p-2 font-bold">
-                <h2>Messages</h2>
-            </div>
-            <div className="p-5">
-            <ul>
-    {messages.map((message) => (
-        <li key={message.id} className="py-2 px-4">
-            {message.sender === "student" ? (
-                // Receiver's message
-                <>
-                    <p className="max-w-60 font-bold text-sm text-left">
-                        Sender: {message.chat_id}
-                    </p>
-                    <div className="p-4 bg-gray-200 max-w-64 rounded-xl">
-                        <p>{message.message}</p>
-                    </div>
-                </>
-            ) : message.sender === "teacher" ? (
-                // Sender's message
-                <>
-                    <p className="max-w-60 font-bold text-sm">
-                        Receiver:
-                    </p>
-                    <div className="p-4 bg-blue-200 max-w-64 rounded-xl">
-                        <p>{message.message}</p>
-                    </div>
-                </>
-            ) : null /* Handle any other type of message sender */}
-            <p className="max-w-60 text-xs text-gray-400 px-2">
-                {new Date(message.timestamp).toLocaleString()}
-            </p>
-        </li>
-    ))}
-</ul>
-
-
-                <div className="mt-5">
-                    <form onSubmit={handleSendMessage}>
-                        <label htmlFor="chat" className="sr-only">
-                            Your message
-                        </label>
-                        <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-                            <textarea
-                                id="chat"
-                                role="1"
-                                className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Your message..."
-                                value={newMessage}
-                                onChange={(e) => setNewMessage(e.target.value)}
-                            ></textarea>
-                            <button
-                                type="submit"
-                                className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
-                            >
-                                <div>
-                                    <BsFillSendFill className="size-5" />
-                                </div>
-                                <span className="sr-only">Send message</span>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
 };
+
+export default Index;
