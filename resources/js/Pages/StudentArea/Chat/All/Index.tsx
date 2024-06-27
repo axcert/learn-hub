8db39fd03c447e92f1e76cff21bb15887f8913da
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-06-27 15:51:42 +0530

    feat: fic chats function error

--- resources/js/Pages/StudentArea/Chat/All/Index.tsx
+++ resources/js/Pages/StudentArea/Chat/All/Index.tsx
@@ -3,6 +3,7 @@ import { Head } from "@inertiajs/react";
 import StudentLayout from "@/Layouts/StudentLayout";
 import img from "@/../../public/asstts/img/girl.jpg";
 import { PageProps } from "@/types";
+import { BsFillSendFill } from "react-icons/bs";
 
 interface Chat {
   teacher: {
@@ -196,15 +197,9 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
                 type="submit"
                 className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
               >
-                <svg
-                  className="w-5 h-5 rotate-90 rtl:-rotate-90"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 20"
-                >
-                  <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                </svg>
+                <div>
+                    <BsFillSendFill className="size-5"/>
+                </div>
                 <span className="sr-only">Send message</span>
               </button>
             </div>
