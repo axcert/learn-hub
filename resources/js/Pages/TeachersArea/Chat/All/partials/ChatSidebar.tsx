9efc259c7054858f167aff6ebd5ef7527d304f1d
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-07-08 17:47:47 +0530

    feat:teacher search function create done

--- resources/js/Pages/TeachersArea/Chat/All/partials/ChatSidebar.tsx
+++ resources/js/Pages/TeachersArea/Chat/All/partials/ChatSidebar.tsx
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import img from "@/../../public/asstts/img/girl.jpg";
 
-
-export function ChatSidebar({ 
+export function ChatSidebar({
     chats,
     onSelectChat,
     onSelectedChatId,
-}:{
-    chats:any ;
-    onSelectChat:(messages: any)=>void;
-    onSelectedChatId:(id:any)=>void;
+}: {
+    chats: any;
+    onSelectChat: (messages: any) => void;
+    onSelectedChatId: (id: any) => void;
 }) {
 
 
 
+    const [searchTerm, setSearchTerm] = useState("");
+    
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
-    function setItems(chat:any){
+    const filteredChats = chats?.filter((chat: any) => {
+        const studentName = chat?.user?.name.toLowerCase();
+        const studentEmail = chat?.user?.email.toLowerCase();
+        const term = searchTerm.toLowerCase();
+        return studentName.includes(term) || studentEmail.includes(term);
+    });
+
+    function setItems(chat: any) {
         onSelectChat(chat.messages);
         onSelectedChatId(chat.id);
     }
@@ -25,13 +36,17 @@ export function ChatSidebar({
             <div className="p-4">
                 <h2 className="font-bold text-xl mb-4">Tutor Chat Panel</h2>
                 <input
+                  onClick={() => {}}
+                  onChange={handleSearchChange}
+                  value={searchTerm}
                     type="text"
                     placeholder="Search"
                     className="mt-2 p-2 w-full border rounded focus:outline-none focus:border-blue-500"
                 />
                 <ul className="mt-4 border-spacing-3 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {chats && chats?.length > 0 ? (
-                        chats?.map((chat: any) => (
+
+                  {filteredChats && filteredChats?.length > 0 ? (
+                        filteredChats?.map((chat: any) => (
                             <li
                                 className="flex justify-between py-5"
                                 key={chat?.id}
@@ -49,29 +64,17 @@ export function ChatSidebar({
                                                 {chat?.user?.name}
                                             </p>
                                             <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                                            {chat?.user?.email}
+                                                {chat?.user?.email}
                                             </p>
                                         </div>
                                     </div>
-                                    {/* <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end p-2">
-                                        <p className="text-sm leading-6 text-gray-900">
-                                            {chat?.teacher?.bio}
-                                        </p>
-                                        <div className="mt-1 flex items-center gap-x-1.5">
-                                            <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                                <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
-                                            </div>
-                                            <p className="text-xs leading-5 text-gray-500">
-                                                Online
-                                            </p>
-                                        </div>
-                                    </div> */}
+                                  
                                 </div>
                             </li>
                         ))
                     ) : (
                         <li className="py-2 px-4">No chats available</li>
-                    )}
+                    )} 
                 </ul>
             </div>
         </div>
